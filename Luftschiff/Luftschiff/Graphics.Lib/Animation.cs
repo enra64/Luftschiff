60f8b378b17ae111c3a078cf96c12fcca85d3664
gSilas <d.micheel@web.de>
2015-04-15 16:26:35 +0200

    stolen from my old repo

---
+++ Luftschiff/Luftschiff/Graphics.Lib/Animation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SFML;
+using SFML.Window;
+using SFML.Graphics;
+
+namespace SoloGame
+{
+    class Animation
+    {
+        private List<IntRect> m_frames = new List<IntRect>();
+        private Texture m_texture;
+
+        public Animation()
+        {
+
+        }
+
+        public void addFrame(IntRect rect)
+        {
+            m_frames.Add(rect);
+        }
+
+        public IntRect getFrame(int n)
+        {
+            return m_frames[n];
+        }
+
+        public Texture Spritesheet
+        {
+            get { return m_texture; }
+            set { m_texture = value; }
+            
+        }
+
+        public int getSize()
+        {
+            return m_frames.Count;
+        }
+    }
+}
