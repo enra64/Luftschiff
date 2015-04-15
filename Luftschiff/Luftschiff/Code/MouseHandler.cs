912a634e881d98975c7bafbc1141ba05223bdf6d
JOCP9733 <janoleclemens@gmx.de>
2015-04-15 19:56:53 +0200

    Added Entity class prototype

--- Luftschiff/Luftschiff/Code/MouseHandler.cs
+++ Luftschiff/Luftschiff/Code/MouseHandler.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SFML.System;
 
 namespace Luftschiff {
     static class MouseHandler {
