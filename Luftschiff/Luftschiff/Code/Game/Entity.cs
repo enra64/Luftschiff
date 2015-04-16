9cacc9f376168ee115a5d1ec4d06f684f5297244
enra64 <sujomear@arcor.de>
2015-04-16 23:09:02 +0200

    am bored. removed using-statements that are not needed

--- Luftschiff/Luftschiff/Code/Game/Entity.cs
+++ Luftschiff/Luftschiff/Code/Game/Entity.cs
@@ -2,10 +2,6 @@
 using SFML.Graphics;
 using SFML.System;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Luftschiff.Code.Game
 {
