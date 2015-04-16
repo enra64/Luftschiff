6183672cff377329b536f392c10f3663daa27695
JOCP9733 <jan-ole.perschewski@st.ovgu.de>
2015-04-16 10:54:53 +0200

    added classes for area and room and started working on the Room class

---
+++ Luftschiff/Luftschiff/Code/Game/AreavRooms/Area.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luftschiff.Code.Game.AreavRooms
+{
+    class Area : Entity
+    {
+        //list to fill with rooms;
+        private List<Room> rooms = new List<Room>();
+    }
+}
