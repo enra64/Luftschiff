73c017d9405f89bb83051d1bacf4a6e46f732d90
gSilas <d.micheel@web.de>
2015-04-17 23:00:59 +0200

    renamed particle particle emitter

--- Luftschiff/Luftschiff/Graphics.Lib/Particle.cs
+++ Luftschiff/Luftschiff/Graphics.Lib/Particle.cs
@@ -5,21 +5,47 @@ using SFML.System;
 
 namespace Luftschiff.Graphics.Lib
 {
-    internal class Particle
+    internal class ParticleEmitter
     {
-        private Clock _clock; 
+        private Clock _clock;
         private Color _color;
         private char _dissolutionRate;
         private bool _dissolve;
-        private Vector2f _gravity; 
+        private Vector2f _gravity;
         private Image _image;
-        private List<Particle> _particleList;
+        private int _particleIterator;
+        private List<ParticleEmitter> _particleList;
         private float _particleSpeed;
-        private Vector2f _position; 
-        private Random _randomizer; 
+        private Vector2f _position;
+        private Random _randomizer;
         private char _shape;
         private Sprite _sprite;
-        private Color _transparent; 
+        private Color _transparent;
         private Vector2f _velocity;
+
+        public ParticleEmitter(int width, int height)
+        {
+        }
+
+        public void Fuel(int particles)
+        {
+        }
+
+        public void Update()
+        {
+        }
+
+        public void Render()
+        {
+        }
+
+        public void Remove()
+        {
+        }
+
+        public string ParticleNumber()
+        {
+            return _particleList.Count.ToString();
+        }
     }
 }
