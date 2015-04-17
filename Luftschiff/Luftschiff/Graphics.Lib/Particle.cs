6b4e2118f81c72e3f36c0c9c6b711c707d3d7345
gSilas <d.micheel@web.de>
2015-04-18 00:40:56 +0200

    particle

---
+++ Luftschiff/Luftschiff/Graphics.Lib/Particle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.System;
+
+namespace Luftschiff.Graphics.Lib
+{
+    internal class Particle : Sprite, Drawable
+    {
+        private static Random _randomizer;
+        private static Clock _lifeClock;
+        private readonly Shape _circle;
+        private readonly Vector2f _position;
+
+        public Particle(Time aliveTime, float radius, Color color)
+        {
+            _randomizer = new Random();
+            LifeTime =
+                Time.FromSeconds(aliveTime.AsSeconds()*(_randomizer.Next(0, 2) + (float) _randomizer.NextDouble()));
+            _circle = new CircleShape(radius);
+            LifeClock = new Clock();
+            Color = color;
+            _position = new Vector2f(100, 100);
+        }
+
+        public Time LifeTime { get; set; }
+
+        public Clock LifeClock
+        {
+            get { return _lifeClock; }
+            set { _lifeClock = value; }
+        }
+
+        public Color Color { get; set; }
+
+        public void Draw(RenderTarget target, RenderStates states)
+        {
+            _circle.FillColor = Color;
+            _circle.Draw(target, states);
+        }
+
+        public void Update()
+        {
+            Position = _position;
+        }
+    }
+}
---
+++ Luftschiff/Luftschiff/Graphics.Lib/ParticleHandler.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters;
+using SFML.Graphics;
+using SFML.System;
+
+namespace Luftschiff.Graphics.Lib
+{
+    internal class ParticleHandler
+    {
+        private static RenderWindow _win;
+        private static List<Particle> _particleKeeper;
+
+        public ParticleHandler(int number)
+        {
+            _particleKeeper = new List<Particle>(number);
+            for (var i = 0; i <= number; i++)
+            {
+                var par = new Particle(Time.FromSeconds(10), 10f, Color.Magenta);
+                Add(par);
+            }
+            
+            _win = Controller.Window;
+        }
+
+        private static void Add(Particle particle)
+        {
+            _particleKeeper.Add(particle);
+        }
+
+        private static void Remove()
+        {
+            _particleKeeper.RemoveAll(par => par.LifeTime == par.LifeClock.ElapsedTime);
+        }
+
+        public void Update()
+        {
+            foreach (var listedParticle in _particleKeeper)
+            {
+                listedParticle.Update();
+            }
+            if (MouseHandler.UnhandledClick)
+            {
+                var par = new Particle(Time.FromSeconds(10), 10f, Color.Magenta);
+                Add(par);
+            }
+            Remove();
+        }
+
+        public void Draw()
+        {
+            foreach (var listedParticle in _particleKeeper)
+            {
+                _win.Draw(listedParticle);
+            }
+        }
+    }
+}
